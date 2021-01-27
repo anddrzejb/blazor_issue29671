5fd9a635658af8664e6a9d83b5fb29704e01dbea
anddrzejb <andrzej@neelyc.com.cy>
2021-01-27 20:46:42 +0100

    Initial commit

---
+++ Pages/ForwardRef.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorApp.Pages
+{
+    public class ForwardRef
+    {
+        private ElementReference _current;
+        public ElementReference Current
+        {
+            get => _current;
+            set
+            {
+                Set(value);
+                //this is just for debugging purpose
+                Console.WriteLine($"Ref: {value.Id ?? "null"}");
+            }
+        }
+        public void Set(ElementReference value) => _current = value;
+    }
+}
